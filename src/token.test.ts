4842079003d8ab0609f6880e3b23f4ed58a70bab
Jan Benes <jan@Jans-MacBook-Air-2.local>
2023-11-13 14:59:14 +0300

    WIP

---
+++ src/token.test.ts
@@ -0,0 +1,78 @@
+import {
+    AztecAddress,
+    AztecNode,
+    CompleteAddress,
+    EthAddress,
+    ExtendedNote,
+    Fr,
+    GrumpkinScalar,
+    Note,
+    PXE,
+    TxStatus,
+    Wallet,
+    computeMessageSecretHash,
+    createPXEClient,
+    getSandboxAccountsWallets,
+    getUnsafeSchnorrAccount,
+    retryUntil,
+    waitForSandbox,
+} from '@aztec/aztec.js';
+
+import { jest } from '@jest/globals';
+import { TokenContract } from './types/Token';
+
+const { PXE_URL = 'http://localhost:8080', AZTEC_NODE_URL = 'http://localhost:8079' } = process.env;
+
+const TIMEOUT = 60_000;
+
+describe('token', () => {
+    jest.setTimeout(TIMEOUT);
+
+    let aztecNode: AztecNode | undefined;
+    let pxe: PXE;
+    let walletA: Wallet;
+    let walletB: Wallet;
+    let userA: CompleteAddress;
+    let userB: CompleteAddress;
+
+    let contract: TokenContract;
+
+    beforeAll(async () => {
+        pxe = createPXEClient(PXE_URL);
+
+        console.log(`Waiting for PXE to be ready on ${PXE_URL}...`)
+        await waitForSandbox(pxe);
+    
+        console.log(`PXE is ready on ${PXE_URL}!`);
+
+        const accounts = await getSandboxAccountsWallets(pxe);
+        const walletA = accounts[0];
+        const walletB = accounts[1];
+        console.log(walletB);
+        const userA = walletA.getAddress();
+        const userB = walletB.getAddress();
+
+        contract = await TokenContract.deploy(walletA, 1000n, userA).send().deployed();
+        console.log("Deployed token at ", contract.address.toString());
+    }, 100_000);
+
+    const awaitServerSynchronized = async (server: PXE) => {
+        const isServerSynchronized = async () => {
+            return await server.isGlobalStateSynchronized();
+        };
+        await retryUntil(isServerSynchronized, 'server sync', 10);
+    };
+
+    it('mint', async () => {
+        console.log(userB);
+        await contract.methods.mint(100n, userB.address).send().wait();
+    });
+
+    it('transfer', async () => {
+
+    });
+
+    it('transfer authwit', async () => {
+
+    });
+});
